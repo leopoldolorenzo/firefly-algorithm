c77df592f706401b8ae42bf25308d8283a257820
leolorenzo <118105550+leopoldolorenzo@users.noreply.github.com>
2024-06-04 21:56:07 +0200

    adaptado

--- sffa.py
+++ sffa.py
@@ -1,84 +1,74 @@
 import numpy as np
 import benchmark
 
-POP_SIZE    = 90
-DIM_SIZE    = 30
-MAX_GEN     = 50
-ALPHA       = 0.2
-BETA_MIN    = 1.0
-GAMMA       = 1.0
-BOUND       = 100
+TAM_POBLACION = 90
+DIMENSION = 30
+MAX_GEN = 50
+ALPHA = 0.2
+BETA_MIN = 1.0
+GAMMA = 1.0
+LIMITE = 100
 
-LB = -BOUND * np.ones(DIM_SIZE)
-UB = BOUND * np.ones(DIM_SIZE)
+LB = -LIMITE * np.ones(DIMENSION)
+UB = LIMITE * np.ones(DIMENSION)
 
-OBJ_FUNC    = benchmark.sphere
+FUNCION_OBJETIVO = benchmark.esfera
 
-# total number of function evaluations
-eval_num = POP_SIZE * MAX_GEN
+# número total de evaluaciones de la función
+num_eval = TAM_POBLACION * MAX_GEN
 
-Best = []
+Mejor = []
 
-# generate fireflies
-def generate_fireflies():
-    return np.random.uniform(0, 1, (POP_SIZE, DIM_SIZE)) * (UB - LB) + LB
+# generar luciérnagas
+def generar_luciernagas():
+    return np.random.uniform(0, 1, (TAM_POBLACION, DIMENSION)) * (UB - LB) + LB
 
-# def objective_function(firefly):
-#     return sum(x ** 2 for x in firefly)
+def calcular_aptitudes(luciernagas):
+    return [FUNCION_OBJETIVO(i) for i in luciernagas]
 
-def calculate_fitnesses(fireflies):
-    return [OBJ_FUNC(i) for i in fireflies]
-    
-def find_limits(fireflies):
-    for i in range(POP_SIZE):
-        np.where(fireflies[i] > BOUND, BOUND, fireflies[i])
-        np.where(fireflies[i] < -BOUND, -BOUND, fireflies[i])
+def encontrar_limites(luciernagas):
+    for i in range(TAM_POBLACION):
+        np.where(luciernagas[i] > LIMITE, LIMITE, luciernagas[i])
+        np.where(luciernagas[i] < -LIMITE, -LIMITE, luciernagas[i])
 
-def update_alpha(alpha):
+def actualizar_alpha(alpha):
     delta = 1 - (10 ** (-4) / 0.9) ** (1 / MAX_GEN)
     return (1 - delta) * alpha
 
-
-fireflies = generate_fireflies()
+luciérnagas = generar_luciernagas()
 
 for gen in range(MAX_GEN):
     
-    ALPHA = update_alpha(ALPHA)
+    ALPHA = actualizar_alpha(ALPHA)
 
-    fitnesses = calculate_fitnesses(fireflies)
-    sorted_index = np.argsort(fitnesses)
-    fitnesses.sort()
+    aptitudes = calcular_aptitudes(luciérnagas)
+    indice_ordenado = np.argsort(aptitudes)
+    aptitudes.sort()
 
-    # temp_fireflies = fireflies.copy()
+    luciérnagas = luciérnagas[indice_ordenado, :]
 
-    fireflies = fireflies[sorted_index, :]
+    luciérnagas_viejas = luciérnagas.copy()
+    aptitudes_viejas = aptitudes.copy()
 
-    fireflies_old = fireflies.copy()
-    fitnesses_old = fitnesses.copy()
+    mejor_luciérnaga = luciérnagas[0]
+    mejor_aptitud = aptitudes[0]
 
-    best_firefly = fireflies[0]
-    best_fitness = fitnesses[0]
-
-    scale = abs(UB - LB)
-    for i in range(POP_SIZE):
-        for j in range(POP_SIZE):
+    escala = abs(UB - LB)
+    for i in range(TAM_POBLACION):
+        for j in range(TAM_POBLACION):
             
-            distance = np.sqrt(np.sum((fireflies[i] - fireflies_old[j]) ** 2))
-            if (fitnesses[i] > fitnesses_old[j]):
+            distancia = np.sqrt(np.sum((luciérnagas[i] - luciérnagas_viejas[j]) ** 2))
+            if (aptitudes[i] > aptitudes_viejas[j]):
                 beta0 = 1
-                beta = (beta0 - BETA_MIN) * np.exp(-GAMMA * (distance ** 2)) + BETA_MIN
-                tmpf = ALPHA * (np.random.rand(DIM_SIZE) - 0.5) * scale
+                beta = (beta0 - BETA_MIN) * np.exp(-GAMMA * (distancia ** 2)) + BETA_MIN
+                tmpf = ALPHA * (np.random.rand(DIMENSION) - 0.5) * escala
                 
-                fireflies[i] = fireflies[i] * (1 - beta) + fireflies[j] * beta + tmpf
+                luciérnagas[i] = luciérnagas[i] * (1 - beta) + luciérnagas[j] * beta + tmpf
 
-    find_limits(fireflies)
-    # fireflies = find_bounds(fireflies)
+    encontrar_limites(luciérnagas)
 
+    Mejor.append(mejor_aptitud)
+    print(f'Generación: {gen} - Mejor: {mejor_aptitud}')
+print(f'Generación: {gen} - Mejor: {mejor_aptitud}')
 
-    Best.append(best_fitness)
-    print(f'Gen: {gen} - Best: {best_fitness}')
-print(f'Gen: {gen} - Best: {best_fitness}')
-
-
-print('end of run')
-
+print('fin de la ejecución')
